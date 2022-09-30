45189b2fe8b5362d26509b20ae6dc2eaced63b64
Ilnaz <72466445@mail.ru>
2022-09-30 23:34:01 +0500

    рабочая программа

--- E001_000/Program.cs
+++ E001_000/Program.cs
@@ -13,77 +13,98 @@
 //Ответ 11-12
 
 int[] Counter = new int[9];
-for(int d =0;d<100;d++)
+for (int d = 0; d < 100; d++)
 {
-int HourOpen = 0;
-int HourClose = 0;
-HourOpen = new Random().Next(8, 16);
-HourClose = new Random().Next(HourOpen + 1, 16);
+    int HourOpen = 0;
+    int HourClose = 0;
+    HourOpen = new Random().Next(8, 16);
+    HourClose = new Random().Next(HourOpen + 1, 16);
 
-Console.WriteLine("время входа " + HourOpen);
-Console.WriteLine("время выхода " + HourClose);
+    Console.WriteLine("время входа " + HourOpen);
+    Console.WriteLine("время выхода " + HourClose);
 
-int count = 2;
-for (int i = HourOpen + 1; i < HourClose; i++)
-{
-    count++;
+    int count = 2;
+    for (int i = HourOpen + 1; i < HourClose; i++)
+    {
+        count++;
 
-};
-System.Console.WriteLine("количество часов внутри " + count);
-int[] Time = new int[count];
-int a = HourOpen - 1;
-Console.Write("в какие часы был внутри ");
-System.Console.WriteLine();
-for (int j = 0; j < count; j++)
-{
-    a = a + 1;
-    Time[j] = a;
-    Console.Write(Time[j] + " ");
+    };
+    System.Console.WriteLine("количество часов внутри " + count);
+    int[] Time = new int[count];
+    int a = HourOpen - 1;
+    Console.Write("в какие часы был внутри ");
+    Console.WriteLine();
+    for (int j = 0; j < count; j++)
+    {
+        a = a + 1;
+        Time[j] = a;
+        Console.Write(Time[j] + " ");
+    }
+    System.Console.WriteLine();
+
+    for (int f = 0; f < count; f++)
+    {
+        if (Time[f] == 8)
+        {
+            Counter[0]++;
+        }
+        else if (Time[f] == 9)
+        {
+            Counter[1]++;
+        }
+        else if (Time[f] == 10)
+        {
+            Counter[2]++;
+        }
+        else if (Time[f] == 11)
+        {
+            Counter[3]++;
+        }
+        else if (Time[f] == 12)
+        {
+            Counter[4]++;
+        }
+        else if (Time[f] == 13)
+        {
+            Counter[5]++;
+        }
+        else if (Time[f] == 14)
+        {
+            Counter[6]++;
+        }
+        else if (Time[f] == 15)
+        {
+            Counter[7]++;
+        }
+        else if (Time[f] == 16)
+        {
+            Counter[8]++;
+        }
+
+    }
 }
-
-for(int f = 0;f<count;f++)
+Console.WriteLine();
+for (int g = 0; g < 9; g++)
+    Console.WriteLine($"{"счетчик по часам "}  {g+8}  {Counter[g]}");
+int maxCounter = 0;
+for (int s = 0; s < 9; s++)
 {
-    if(Time[f] == 8)
+    if (maxCounter < Counter[s])
     {
-        Counter[0]++;
+        maxCounter = Counter[s];
     }
-    else if(Time[f]==9)
+}
+for (int i = 0; i < 9; i++)
+{
+    if (maxCounter == Counter[i])
     {
-        Counter[1]++;
+        int maxHour = i + 8;
+        int maxHour1 = i + 9;
+        System.Console.WriteLine();
+        Console.Write("время максимального количество людей  " + maxHour);
+        Console.Write(" " + maxHour1);
     }
-    else if(Time[f]==10)
-    {
-        Counter[2]++;
-    }
-    else if(Time[f]==11)
-    {
-        Counter[3]++;
-    }
-    else if(Time[f]==12)
-    {
-        Counter[4]++;
-    }
-    else if(Time[f]==13)
-    {
-        Counter[5]++;
-    }
-    else if(Time[f]==14)
-    {
-        Counter[6]++;
-    }
-    else if(Time[f]==15)
-    {
-        Counter[7]++;
-    }
-    else if(Time[f]==16)
-    {
-        Counter[8]++;
-    }
-    
-}}
-System.Console.WriteLine();
-for(int g=0;g<9;g++)
-Console.WriteLine("счетчик по часам  " + Counter[g]);
+}
 
 
 
